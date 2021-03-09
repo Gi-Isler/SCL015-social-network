847945364b4bf97cf7ed8a5cbf801140333b2655
Gi-Isler <giselaisler25@gmail.com>
2021-03-09 13:02:16 -0300

    boton google login

--- src/lib/view/templateLogin.js
+++ src/lib/view/templateLogin.js
@@ -1,20 +1,24 @@
+import { loginGoo } from '../index.js';
+
 export const login = () => {
   const divLogin = document.createElement('div');
   const viewLogin = ` 
-  <div id="conteinerLogin">  
-  <div id="containerBaseLogin">
-    <input id="inputImgLogo" type="image" src="images/logo.png" class="img">
-    <input type="nombreUsuario" id="nombreUsuario" name="usuario" placeholder="Nombre Usuario"/>
-    <input type="password" id="password" name="Password" placeholder="Password"minlength="6" maxlength="8" required>
-    <button id="btnEnter" class="btnEnter">Entrar</button>
-    <button id="recoverPassword" class="recoverPassword">Recuperar mi contraseña</button>
-    <h3>Ingresa con tus redes</h3>
-    <input id="inputImgGoogle" type="image" src="images/initGoo.png" class="boton">
-    <button id="btnCheckIn" class="btnCheckIn">Registrarme</button>  
+    <div id="containerBaseLogin">
+      <img id="inputImgLogo" src="images/logo.png" class="img">
+      <input type="nombreUsuario" id="nombreUsuario" class="inputlogin" name="usuario" placeholder="Nombre Usuario"/>
+      <input type="password" id="password"class="inputlogin" name="Password" placeholder="Password"minlength="6" maxlength="8" required>
+      <button id="btnEnter" class="btnEnter">Entrar</button>
+      <button id="recoverPassword" class="recoverPassword">Recuperar mi contraseña</button>
+      <h3>Ingresa con tus redes</h3>
+      <img id="botonGoogle" class="botonGoogle" src="images/initGoo.png" type="button">
+      <button id="btnCheckIn" class="btnCheckIn">Registrarme</button>  
     </div>
-    </div> 
     `;
   divLogin.innerHTML = viewLogin;
+  const btnGoogle = divLogin.querySelector('#botonGoogle');
+ btnGoogle.addEventListener('click', () => {
+  loginGoo()
+ })
   const restroForm = divLogin.querySelector('#btnCheckIn');
   restroForm.addEventListener('click', () => {
     window.location.href = '#/registro';
