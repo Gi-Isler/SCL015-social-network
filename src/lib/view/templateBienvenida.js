efa051224307dc328d7d95c7a0bd72991a4ecf78
Gi-Isler <giselaisler25@gmail.com>
2021-03-10 10:27:51 -0300

    estilos icono bienvenida

--- src/lib/view/templateBienvenida.js
+++ src/lib/view/templateBienvenida.js
@@ -1,14 +1,25 @@
 export const bienvenida = () => {
-    const divBienvenida = document.createElement('div');
-    const viewBienvenida = ` 
-    
-     <h1>Bienvenidos a nuestra APP </h1>
-    
+  const divBienvenida = document.createElement('div');
+  const viewBienvenida = ` 
+    <div id="containerBaseLogin" class="containerBase">
+      <nav>
+        <div class="search">
+          <img src="images/Rectangle.png" id="logoWelcome" class="logoWelcome">
+          <input placeholder="Buscar">
+            <ul>
+              <li><a href="#"><img id="home" src="images/home.png" type="button"></a></li>
+              <li><a href="#"><img id="group" src="images/account_group.png" type="button"></a></li>
+              <li><a href="#"><img id="maps" src="images/google_maps.png" type="button"></a></li>
+              <li><a href="#"><img id="msn" src="images/carta mensaje 1.png" type="button"></a></li>
+            </ul>    
+        </div>
+      </nav> 
+      <h1>Bienvenidos a nuestra APP </h1>
+    </div>
     `;
+ 
+  divBienvenida.innerHTML = viewBienvenida;
 
-  
-divBienvenida.innerHTML = viewBienvenida;
 
-
-return divBienvenida;
-  }
+  return divBienvenida;
+}
--- src/lib/view/templateLogin.js
+++ src/lib/view/templateLogin.js
@@ -3,7 +3,7 @@ import { loginGoo } from '../index.js';
 export const login = () => {
   const divLogin = document.createElement('div');
   const viewLogin = ` 
-    <div id="containerBaseLogin">
+    <div id="containerBaseLogin" class="containerBase">
       <img id="inputImgLogo" src="images/logo.png" class="img">
       <input type="nombreUsuario" id="nombreUsuario" class="inputlogin" name="usuario" placeholder="Nombre Usuario"/>
       <input type="password" id="password"class="inputlogin" name="Password" placeholder="Password"minlength="6" maxlength="8" required>
@@ -16,13 +16,13 @@ export const login = () => {
     `;
   divLogin.innerHTML = viewLogin;
   const btnGoogle = divLogin.querySelector('#botonGoogle');
- btnGoogle.addEventListener('click', () => {
-  loginGoo()
- })
+  btnGoogle.addEventListener('click', () => {
+    loginGoo()
+  });
   const restroForm = divLogin.querySelector('#btnCheckIn');
   restroForm.addEventListener('click', () => {
     window.location.href = '#/registro';
-  })
+  });
 
-  return divLogin;
+ return divLogin;
 };
